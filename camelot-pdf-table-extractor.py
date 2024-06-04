2950af642141cae18f7dc48ca42a60c30ed76a72
akfike <akfike@crimson.ua.edu>
2024-06-04 11:50:10 -0500

    add another page

--- camelot-pdf-table-extractor.py
+++ camelot-pdf-table-extractor.py
@@ -7,7 +7,7 @@ print(find_library("gs")) # Make sure you have ghostscript installed (thru homeb
 pdf_path = "pdfs/NSDUH-2022-DS0001-info-codebook (1).pdf"
 
 # Extract tables using stream mode (for tables without borders)
-tables = camelot.read_pdf(pdf_path, pages='48,528,529', flavor='stream', edge_tol=1000)
+tables = camelot.read_pdf(pdf_path, pages='48,514,528,529', flavor='stream', edge_tol=1000)
 
 # # Print the tables or save them to CSV files
 for i, table in enumerate(tables):
@@ -16,5 +16,5 @@ for i, table in enumerate(tables):
 	camelot.plot(table, kind='contour') # text, grid, contour, line, joint, ...
 	plt.show()
 	print(table.df)
-	table.to_csv(f"camelot_table_{i}.csv")
+	table.to_csv(f"csvs/camelot_table_{i}.csv")
 	input("Hit Enter for next table")
