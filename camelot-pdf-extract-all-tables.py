d7cb6ebb230a7326d4317d9887caf9c45259293e
akfike <akfike@crimson.ua.edu>
2024-07-31 11:46:34 -0500

    nhes stuff

--- camelot-pdf-extract-all-tables.py
+++ camelot-pdf-extract-all-tables.py
@@ -4,19 +4,19 @@ from ctypes.util import find_library
 print(find_library("gs")) # Make sure you have ghostscript installed (thru homebrew or other)
 
 # Specify the path to your PDF file
-pdf_path = "pdfs/37692-0001-Codebook.pdf"
+pdf_path = "pdfs/PFI_2019.pdf"
 
 # Extract tables using stream mode (for tables without borders)
 tables = camelot.read_pdf(
     pdf_path, 
-    pages='27-1028', # or all
+    pages='all', # or all
     flavor='stream', # or lattice
     edge_tol=1000, 
     # table_regions=['55,702,300,52'], # x1, y1, x2, y2 (top left corner, bottom right corner)
     flag_size=True
 )
 
-# camelot.plot(tables[0], kind='textedge').show()
+camelot.plot(tables[0], kind='textedge').show()
 
 # Function to convert specific columns to strings to avoid automatic date conversion
 def convert_columns_to_string(df):
@@ -29,6 +29,6 @@ for i, table in enumerate(tables):
     print(f"Table {i}")
     print(table.parsing_report)
     table.df = convert_columns_to_string(table.df)
-    table.to_csv(f"csvs/spi_table_{i}.csv")
+    table.to_csv(f"csvs/raw_csvs/nhes_pfi_table_{i}.csv")
 
-    # plt.show()
+    plt.show()
--- extract_variable_names_csv.py
+++ extract_variable_names_csv.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def extract_variable_names():
     # Hardcoded paths for the CSV file and the output file
-    csv_file_path = 'datasets/NSSATS_PUF_2020_CSV.csv'
-    output_file_path = 'nsatts_variables.txt'
+    csv_file_path = 'datasets/PFI_2019_Dataset.csv'
+    output_file_path = 'NHES_PFI_2019_variable_names.txt'
 
     # Load the CSV file with a specific encoding
     try:
